a399734e262609a2c4c9e8db1b3cb9057e12d805
yovi.putra <yovi.putra@multipolar.com>
2020-05-17 13:32:01 +0700

    Initial Commit

---
+++ medsos_authentication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  medsos_authentication
+//
+//  Created by yoviekaputra on 17/05/20.
+//  Copyright © 2020 gyosanila. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
